bd8ed9a9a114345dfcb44d9530b0e7ab3fd2227b
nas22663 <70116321+nas22663@users.noreply.github.com>
2024-04-23 20:22:56 +0200

    late

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import { initiateApp } from "./Server/src/initiate-app.js";
 
-config({ path: "/Server/config/dev.config.env" });
+config({ path: "Server/config/dev.config.env" });
 
 const app = express();
 
